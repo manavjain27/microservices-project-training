417fe8709f56b33fc8d421156bc4682054b39af9
manavjain27 <jain.manav277@gmail.com>
2021-06-29 20:43:18 +0530

    Added cloud-server

--- config-server/spring_cloud_config_server/src/test/java/com/wp/microservices/spring_cloud_config_server/SpringCloudConfigServerApplicationTests.java
+++ config-server/spring_cloud_config_server/src/test/java/com/wp/microservices/spring_cloud_config_server/SpringCloudConfigServerApplicationTests.java
@@ -1,4 +1,4 @@
-package com.currency.microservices.spring_cloud_config_server;
+package com.wp.microservices.spring_cloud_config_server;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
